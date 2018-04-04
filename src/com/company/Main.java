05ef43108413a7ae51d57b3c120eee78dc6df5a6
Zach Sharpe <zach.sharpe5@gmail.com>
2018-04-04 17:40:09 -0500

    Groundwork for HSSF Workbook creation method and testing.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,59 @@
 package com.company;
 
-import org.apache.poi.xssf.eventusermodel.XSSFBReader;
-import org.apache.poi.xssf.streaming.SXSSFRow;
-import org.apache.poi.xssf.streaming.SXSSFSheet;
-import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileView;
+import javax.swing.plaf.FileChooserUI;
 import java.io.File;
 import java.io.FileOutputStream;
 
 public class Main {
 
+    public Main() {
+    }
 
-// Creates a new workbook based on file workbook.xls
-    SXSSFWorkbook newSheet(){
+    // Read Excel File and places information into an array.
+    private void readSheet(){
+
+
+    }
+
+    // Creates a new workbook based on file workbook.xls
+    HSSFWorkbook createSheet(){
 
         try {
 // File name and output declaration.
             //TO DO: Change output path.
-            File file = new File("/Users/zachsharpe/IdeaProjects/Metric/ExcelOutput");
+            File file = new File("ExcelFile5.xls");
             FileOutputStream out = new FileOutputStream(file);
 
 // Workbook, sheet, row, and cell creation
-            SXSSFWorkbook workbook = new SXSSFWorkbook();
-            SXSSFSheet sheet = workbook.createSheet();
-            SXSSFRow row = sheet.createRow(0);
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet();
+            HSSFRow row = sheet.createRow(0);
+            HSSFCell cell = row.createCell(0);
+            cell.setCellValue("Zach");
             workbook.write(out);
             return workbook;
 
         }catch (Exception exception){
-            System.out.println(exception);
+            System.out.println("Exception: " + exception);
             exception.printStackTrace();
             return null;
         }
+
 
     }
     public static void main(String[] args) {
 
         Main workbook = new Main();
-        workbook.newSheet();
+        workbook.createSheet();
+        workbook.readSheet();
 
     }
 }
